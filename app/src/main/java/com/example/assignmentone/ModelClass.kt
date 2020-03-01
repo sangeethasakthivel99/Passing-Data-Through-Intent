b90f146c0509cfb3921cfa37545b3b3a39fb0388
Sangeetha <sangeetha.s@coviam.com>
2020-03-01 15:09:20 +0530

    Passing Data Through Intent

--- app/src/main/java/com/example/assignmentone/ModelClass.kt
+++ app/src/main/java/com/example/assignmentone/ModelClass.kt
@@ -1,6 +1,10 @@
 package com.example.assignmentone
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class ModelClass(
     var username : String? = null,
     var password : String? = null
-)
+) : Parcelable
